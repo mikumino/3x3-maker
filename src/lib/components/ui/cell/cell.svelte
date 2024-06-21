38255d167b58e566e6c0af568e129431d47a521e
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-06-21 11:16:12 -0700

    placeholder image

--- src/lib/components/ui/cell/cell.svelte
+++ src/lib/components/ui/cell/cell.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-
+    import Image  from 'lucide-svelte/icons/image';
+    export let image: string = "";
 </script>
 
 <div class="flex flex-col items-center justify-center w-full h-full bg-background">
-
+    {#if image}
+        <img src={image} alt="filled cell" class="w-full h-full" />
+    {/if}
+    {#if !image}
+        <Image class="w-11 h-11" /> 
+    {/if}
 </div>
