d1451d322831b3c70664c92e7894c9bae969902e
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-06-21 11:09:03 -0700

    griddy

---
+++ src/lib/components/ui/cell/cell.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<div class="flex flex-col items-center justify-center w-full h-full bg-background">
+
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Lightswitch from "$lib/components/ui/lightswitch/lightswitch.svelte";
+    import Cell from "$lib/components/ui/cell/cell.svelte";
+</script>
+
+<div class="flex flex-row items-center justify-center h-screen">
+    <div class="hidden flex-col">
+
+    </div>
+    <div class="flex flex-col">
+        <div class="flex flex-col gap-3 w-[600px] h-[600px]">
+            <div class="flex flex-row gap-3 w-full h-full">
+                <div class="w-1/3 h-full">
+                    <Cell />
+                </div>
+                <div class="w-1/3 h-full">
+                    <Cell />
+                </div>
+                <div class="w-1/3 h-full">
+                    <Cell />
+                </div>
+            </div>
+            <div class="flex flex-row gap-3 w-full h-full">
+                <div class="w-1/3 h-full">
+                    <Cell />
+                </div>
+                <div class="w-1/3 h-full">
+                    <Cell />
+                </div>
+                <div class="w-1/3 h-full">
+                    <Cell />
+                </div>
+            </div>
+            <div class="flex flex-row gap-3 w-full h-full">
+                <div class="w-1/3 h-full">
+                    <Cell />
+                </div>
+                <div class="w-1/3 h-full">
+                    <Cell />
+                </div>
+                <div class="w-1/3 h-full">
+                    <Cell />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
