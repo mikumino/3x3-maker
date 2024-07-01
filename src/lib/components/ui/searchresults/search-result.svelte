a0be2c94adf56799d3ce86fde2e78123e5e66939
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-07-01 13:48:15 -0700

    crop button

--- src/lib/components/ui/searchresults/search-result.svelte
+++ src/lib/components/ui/searchresults/search-result.svelte
@@ -2,6 +2,7 @@
     import { quintOut } from "svelte/easing";
     import { fly } from "svelte/transition";
     import { createEventDispatcher } from "svelte";
+	import Button from "../button/button.svelte";
 
     export let result: Result;
 
@@ -11,14 +12,19 @@
         dispatch('resultClick', result);
     }
 
+    const handleCropClick = (e: Event) => {
+        e.stopPropagation();
+        console.log('crop click');
+    }
+
 </script>
 
-<button on:click={handleResultClick} class="aspect-square grid-rows-1 grid-cols-1">
+<button on:click={handleResultClick} class="relative aspect-square grid-rows-1 grid-cols-1 group">
+    <Button on:click={handleCropClick} variant="outline" class="opacity-0 absolute group-hover:opacity-100 transition-all bottom-5 right-5">Crop</Button>  
     <img 
     in:fly={{y: 10, easing: quintOut, duration: 300, delay: 300}} 
     out:fly={{y: 10, easing: quintOut, duration: 300}}
     class="object-cover h-full w-full rounded-sm col-span-1 row-span-1" 
     src={result.imageUrl} 
-    alt={result.title} 
-    />
+    alt={result.title} />
 </button>
